41187321dffc85f98022775d4875b087ef54f79c
oop-cpu <dnathan047@gmail.com>
2025-09-19 08:35:47 -0700

    lazy commit

--- lazycommit.py
+++ lazycommit.py
@@ -3,3 +3,9 @@ import time
 subprocess.run(["git add ."], shell=True)
 time.sleep(1)
 subprocess.run(["git commit -m \"lazy commit\""], shell=True)
+
+file_path = "/home/oop-cpu/gitKey.dat"
+
+with open(file_path, 'r') as file:
+    content = file.read()
+    print(content)
